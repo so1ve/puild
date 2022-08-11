2e83c148052a6625827fd41548e3980624ecbc5f
so1ve <nn_201312@163.com>
2022-08-11 12:18:53 +0800

    refactor: move to picocolors

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,7 +1,7 @@
-import c, { gray } from "picocolors";
+import c from "picocolors";
 
 const currentTime = () => (new Date()).toLocaleTimeString();
 
 export function info (...messages: any[]) {
-  console.log(`\n${c.bold(c.inverse(c.blue(" INFO ")))} [${gray(currentTime())}]`, ...messages);
+  console.log(`\n${c.bold(c.inverse(c.blue(" INFO ")))} [${c.gray(currentTime())}]`, ...messages);
 }
