c53b73d807cd33fcd59d02492db16c4b68f81abd
so1ve <nn_201312@163.com>
2022-08-14 10:53:46 +0800

    chore: update deps

--- src/utils/log.ts
+++
@@ -1,7 +0,0 @@
-import c from "picocolors";
-
-const currentTime = () => (new Date()).toLocaleTimeString();
-
-export function info (...messages: any[]) {
-  console.log(`\n${c.bold(c.inverse(c.blue(" INFO ")))} [${c.gray(currentTime())}]`, ...messages);
-}
