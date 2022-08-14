0e8928a49791ebe8f10c42817a4b02f2dfca65c5
so1ve <nn_201312@163.com>
2022-08-14 20:25:02 +0800

    fix: module

--- src/utils/get-dist-files.ts
+++ src/utils/get-dist-files.ts
@@ -2,7 +2,8 @@ import path from "node:path";
 import type { RollupOutput } from "rollup";
 
 export function getDistFiles (distPath: string, rollupOutputs: RollupOutput[][]) {
-  const fileNames = rollupOutputs.flat()
+  const fileNames = rollupOutputs
+    .flat()
     .map(o => o.output.map(f => f.fileName))
     .flat();
   const distFiles = fileNames.map(f => path.join(distPath, f));
